4da39a573c16023ec15a5a61ec8f425ba703d6a1
liuTingT <17839977109@163.com>
2019-12-02 18:12:33 +0800

    改造admin、backup、monitor项目，将admin和backup作为服务注册到monitor服务的注册中心

--- mango-admin/src/main/java/com/louis/mango/admin/MangoAdminApplication.java
+++ mango-admin/src/main/java/com/louis/mango/admin/MangoAdminApplication.java
@@ -3,9 +3,11 @@ package com.louis.mango.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.louis.mango"})
 @MapperScan("com.louis.mango.**.dao")
+@EnableDiscoveryClient
 public class MangoAdminApplication {
 
 	public static void main(String[] args) {
--- mango-monitor/src/main/java/com/louis/mango/monitor/MangoMonitorApplication.java
+++ mango-monitor/src/main/java/com/louis/mango/monitor/MangoMonitorApplication.java
@@ -2,9 +2,11 @@ package com.louis.mango.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableAdminServer
 public class MangoMonitorApplication {
