474c77f3643acfea364003863f7cefafb7497faa
liuTingt <17839977109@163.com>
2019-12-23 10:35:35 +0800

    添加配置中心、消息总线后

---
+++ mango-config/src/main/java/com/louis/mangoconfig/MangoConfigApplication.java
@@ -0,0 +1,17 @@
+package com.louis.mangoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MangoConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoConfigApplication.class, args);
+	}
+
+}
