670fccb47c56c2cb4b1c085c064149af8f6b628c
liuTingt <17839977109@163.com>
2019-12-23 10:38:56 +0800

    添加配置中心、消息总线后

--- mango-producer2/src/main/java/com/example/mango/producer/MangoProducerApplication.java
+++ mango-producer2/src/main/java/com/example/mango/producer/MangoProducerApplication.java
@@ -2,8 +2,10 @@ package com.example.mango.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MangoProducerApplication {
 
 	public static void main(String[] args) {
